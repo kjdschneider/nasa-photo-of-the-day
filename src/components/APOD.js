159c9da3466a3694d56ccceac85173d17481ce19
KJ Schneider <kjdschneider@gmail.com>
2019-11-19 21:42:26 -0500

    Trying to get video working

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,18 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import DisplayAPOD from './DisplayAPOD';
+import DisplayVideo from './DisplayVideo';
 
 const APOD = () => {
     const [date, setDate] = useState('2012-03-14');
     const [nasa, setNasa] = useState({});
     
 
-    // const today = new Date();
-    // const year = today.getFullYear();
-    // const month = today.getMonth();
-    // const day = today.getDate();
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
 
-    // setDate(`${year}-${month}-${day}`);
+    setDate(`${year}-${month}-${day}`);
 
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
@@ -20,11 +21,19 @@ const APOD = () => {
             .catch(err => console.log(err))
     }, [])
     
-    return (
-        <div>
-            <DisplayAPOD title={nasa.title} apodDate={date} url={nasa.url} explanation={nasa.explanation} copywright={nasa.copywright} />
-        </div>
-    )
+    if (nasa.media_type === 'image') {    
+        return (
+            <div>
+                <DisplayAPOD title={nasa.title} apodDate={date} url={nasa.url} explanation={nasa.explanation} copywright={nasa.copywright} />
+            </div>
+        )
+    } else if (nasa.media_type === 'video') {
+        return (
+            <div>
+                <DisplayVideo title={nasa.title} apodDate={date} url={nasa.url} explanation={nasa.explanation} copywright={nasa.copywright} />
+            </div>
+        )
+    }
 }
 
 export default APOD;
---
+++ src/components/DisplayVideo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Player} from 'video-react';
+
+const DisplayVideo = props => {
+    return (
+        <div>
+            <div className='title'>
+                <h1>Astronomy Video of the Day - {props.title}</h1>
+                <h2 className='date'>{props.apodDate}</h2>
+            </div>
+            <div className='img-contain'>
+                <Player playsInline src={props.url} alt='NASA Astronomy Video of the Day' />
+            </div>
+            <div className='info'>
+                <p className='explanation'>{props.explanation}</p>
+                <p className='copy'>{props.copywright}</p>
+            </div>
+        </div>
+    )
+}
+
+export default DisplayVideo;
